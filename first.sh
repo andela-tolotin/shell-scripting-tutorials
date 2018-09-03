114d465b34439bad10d5bc0caaab837badd3bf37
Temitope Olotin <temitope.olotin@andela.com>
2018-09-03 17:06:52 +0100

    add lesson files

---
+++ first.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a comment
+echo Hello world # This is a comment too
+
+# This is a comment!
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
---
+++ var.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+MY_MESSAGE="Hello Wold"
+
+echo $MY_MESSAGE
+
+echo What is your name?
+read MY_NAME
+echo "Hello $MY_NAME - hope you're well."
